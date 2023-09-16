caca03533dee5a2ee521758c7604d29f4dc96d3d
DianaShylova <torfinn.karlseffny@gmail.com>
2023-09-16 09:54:58 +0300

    my raw hw7 phonebook

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,27 +1,16 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore} from "@reduxjs/toolkit";
 import { contactReducer } from "./contactsSlice";
 import { filterReducer } from "./filterSlice";
-import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER, } from "redux-persist";
-import storage from "redux-persist/lib/storage";
 
-const persistConfig = {
-  key: 'contact',
-  storage,
-}
 
-const persistedReducer = persistReducer(persistConfig, contactReducer);
+
+
+
 
 export const store = configureStore({
   reducer: {
-    contacts: persistedReducer,
+    contacts: contactReducer,
     filter: filterReducer,
   },
-  middleware: getDefaultMiddleware =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }),
 });
 
-export const persistor = persistStore(store);
